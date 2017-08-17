3ef9c0cf1b26ba1a6b8d6afff6f63d4cb9a737d4
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2017-08-17 13:48:57 +0300

    minor changes

--- app/src/main/java/lawrence/blooddonor/activities/MainActivity.java
+++ app/src/main/java/lawrence/blooddonor/activities/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 		final TabsPagerAdapter adapter = new TabsPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
 		viewPager.setAdapter(adapter);
 		viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-		tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+		tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 			@Override
 			public void onTabSelected(TabLayout.Tab tab) {
 				viewPager.setCurrentItem(tab.getPosition());
