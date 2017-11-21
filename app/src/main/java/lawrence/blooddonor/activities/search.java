470d124037ff289fab510741f2620c8d7350f491
Lawrence Karanja <lawrenslava1@gmail.com>
2017-11-21 16:52:13 +0300

    Search Fucntion
    Started creatign the search fucntionality for the app

--- app/src/main/java/lawrence/blooddonor/activities/search.java
+++ app/src/main/java/lawrence/blooddonor/activities/search.java
@@ -1,7 +1,26 @@
 package lawrence.blooddonor.activities;
 
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import lawrence.blooddonor.R;
 
@@ -12,5 +31,141 @@ public class search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem=menu.findItem(R.menu.search_main);
+        //Searchable Config
+        SearchManager searchManager=(SearchManager) getSystemService(getApplicationContext().SEARCH_SERVICE);
+        SearchView searchView=(SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        if (searchItem != null) {
+            searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconified(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id==R.id.action_search){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        // Get search query and create object of class AsyncFetch
+        if(intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query=intent.getStringExtra(SearchManager.QUERY);
+            Object searchView = null;
+            if(searchView !=null) {
+                searchView.clearfocus();
+            }
+            new AsyncFetch(query).execute();
+        }
+    }
+    private class AsyncFetch extends AsyncTask<String, String, String>{
+
+        ProgressDialog progressDialog=new ProgressDialog(search.this);
+        HttpURLConnection conn;
+        URL url=null;
+        String searchQuery;
+
+        public AsyncFetch(String searchQuery){
+            this.searchQuery=searchQuery;
+        }
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+
+            progressDialog.setMessage("\tLoading...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+        @Override
+        protected String doInBackground(String...params) {
+            try{
+                url=new URL("http://192.168.137.1/search.php");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(1000);
+                conn.setConnectTimeout(2500);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput to true as we send and recieve data
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // add parameter to our above url
+                Uri.Builder builder = new Uri.Builder().appendQueryParameter("searchQuery", searchQuery);
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+                    return("Connection error");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
     }
 }
